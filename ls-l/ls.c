30176ae019a2806649105e604dacf7930c486602
Vadim Evard <v.e.evard@gmail.com>
2016-11-21 22:03:53 +0300

    stat: printing uid

--- ls-l/ls.c
+++ ls-l/ls.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
-int main (void)
+#include <sys/stat.h>
+#include <stdint.h>
+
+int print_file_info(const char *path, const struct stat *st)
 {
+    printf("%s %ju\n", path, (uintmax_t) st->st_uid);
+}
+
+int list_directory(const char *directory_name) {
     DIR *dp;
     struct dirent *ep;
+    struct stat st;
 
-    dp = opendir ("./");
+    dp = opendir(directory_name);
     if (dp != NULL)
     {
-        while (ep = readdir (dp))
-            puts(ep->d_name);
+        while (ep = readdir(dp))
+        {
+
+            if (stat(ep->d_name, &st))
+            {
+                perror("stat error!");
+            }
+            print_file_info(ep->d_name, &st);
+        }
 
         closedir(dp);
     }
@@ -18,4 +33,10 @@ int main (void)
         perror ("Couldn't open the directory");
 
     return 0;
+}
+
+int main (void)
+{
+    list_directory(".");
+
 }
