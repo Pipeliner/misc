c966485234439bee1bf6e4d824c006b851e2f59b
Vadim Evard <v.e.evard@gmail.com>
2016-11-23 05:13:26 +0300

    print file type & access rights

--- ls-l/ls.c
+++ ls-l/ls.c
@@ -6,7 +6,61 @@
 
 int print_file_info(const char *path, const struct stat *st)
 {
-    printf("%s %ju\n", path, (uintmax_t) st->st_uid);
+    //printf("%s %ju\n", path, (uintmax_t) st->st_uid);
+    mode_t mode = st->st_mode;
+    mode_t file_type = mode & S_IFMT;
+
+    switch (file_type)
+    {
+        case S_IFREG:
+            putchar('-');
+            break;
+        case S_IFBLK:
+            putchar('b');
+            break;
+        case S_IFCHR:
+            putchar('c');
+            break;
+        case S_IFDIR:
+            putchar('d');
+            break;
+        case S_IFIFO:
+            putchar('p');
+            break;
+        case S_IFLNK:
+            putchar('l');
+            break;
+        case S_IFSOCK:
+            putchar('s');
+            break;
+        default:
+            putchar('?');
+            break;
+    }
+
+
+
+#define PUT_RW_BIT(field, field_char) \
+    putchar(mode & field ? field_char : '-');
+
+#define PUT_X_BIT(field, set_field, field_char, set_field_char) \
+    putchar(mode & set_field ? set_field_char : (mode & field ? field_char : '-'));
+
+    PUT_RW_BIT(S_IRUSR, 'r');
+    PUT_RW_BIT(S_IWUSR, 'w');
+    PUT_X_BIT(S_IXUSR, S_ISUID, 'x', 'S');
+
+    PUT_RW_BIT(S_IRGRP, 'r');
+    PUT_RW_BIT(S_IWGRP, 'w');
+    PUT_X_BIT(S_IXGRP, S_ISGID, 'x', 'S');
+
+    PUT_RW_BIT(S_IROTH, 'r');
+    PUT_RW_BIT(S_IWOTH, 'w');
+    PUT_X_BIT(S_IXOTH, S_ISVTX, 'x', 'T');
+
+    putchar(' ');
+
+    puts(path);
 }
 
 int list_directory(const char *directory_name) {
